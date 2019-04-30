8ed29915e2b837bac1283667686e513250657bc9
Evgenii Ryshkov <eryshkov@gmail.com>
2019-04-30 15:17:47 +0300

    reg form is fixed

--- src/Controller/SignUpController.php
+++ src/Controller/SignUpController.php
@@ -2,24 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\SignUpFormType;
+use App\Form\UserRegistrationFormModel;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SignUpController extends AbstractController
 {
     /**
      * @Route("/signup", name="sign_up")
      */
-    public function index(Request $request)
+    public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
         $form = $this->createForm(SignUpFormType::class);
         
         $form->handleRequest($request);
         
         if ($form->isSubmitted() && $form->isValid()) {
-            dd($form->getData());
+            /** @var UserRegistrationFormModel $userModel */
+            $userModel = $form->getData();
+            $user = new User();
+            $user->setEmail($userModel->email);
+            $user->setPassword(
+                $passwordEncoder->encodePassword($user, $userModel->plainPassword)
+            );
         }
         
         return $this->render('sign_up/index.html.twig', [
--- src/Form/UserRegistrationFormModel.php
+++ src/Form/UserRegistrationFormModel.php
@@ -2,9 +2,23 @@
 
 namespace App\Form;
 
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+use Symfony\Component\Validator\Constraints as Assert;
+
 class UserRegistrationFormModel
 {
+    /**
+     * @Assert\NotBlank(message="Введите свой email")
+     * @Assert\Email(message="Неверный email")
+     */
     public $email;
+    /**
+     * @Assert\NotBlank(message="Введите свой пароль")
+     * @Assert\Length(min="6", minMessage="Пароль должен быть не короче 6 символов")
+     */
     public $plainPassword;
+    /**
+     * @Assert\IsTrue(message="Нужно принять условия пользовательского соглашения")
+     */
     public $agreeTerms;
 }
