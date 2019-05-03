c71b0979e9254965e88959d8ff0ed77920a49d7a
Evgenii Ryshkov <eryshkov@gmail.com>
2019-05-03 07:20:19 +0300

    ListDetailsController.php fixed

--- src/Controller/ListDetailsController.php
+++ src/Controller/ListDetailsController.php
@@ -2,19 +2,41 @@
 
 namespace App\Controller;
 
+use App\Repository\WordListRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class ListDetailsController
+ * @IsGranted("ROLE_USER")
+ */
 class ListDetailsController extends BaseController
 {
+    /**
+     * @var WordListRepository
+     */
+    private $wordListRepository;
+    
+    public function __construct(WordListRepository $wordListRepository)
+    {
+        $this->wordListRepository = $wordListRepository;
+    }
+    
     /**
      * @Route("/list/{id}/details", name="app_list_details")
      */
     public function index(int $id)
     {
         $currentUser = $this->getUser();
+    
+        $list = $this->wordListRepository->findOneBy([
+            'user' => $currentUser,
+            'id' => $id,
+        ]);
         
         return $this->render('list_details/index.html.twig', [
             'user' => $currentUser,
+            'list' => $list,
         ]);
     }
 }
