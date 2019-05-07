9b3c10347267158da86cd33138c8a4f892245c91
Evgenii Ryshkov <eryshkov@gmail.com>
2019-05-07 14:21:05 +0300

    AdminPageController.php is fixed

--- src/Controller/AdminPageController.php
+++ src/Controller/AdminPageController.php
@@ -2,13 +2,18 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class AdminPageController
+ * @IsGranted("ROLE_ADMIN")
+ */
 class AdminPageController extends AbstractController
 {
     /**
-     * @Route("/admin/page", name="admin_page")
+     * @Route("/admin", name="app_admin")
      */
     public function index()
     {
