c5f273903937e14903ded0a9a509d4bbf7020608
Evgenii Ryshkov <eryshkov@gmail.com>
2019-05-09 01:16:22 +0300

    WordEditFormType.php fixed

--- src/Form/WordEditFormType.php
+++ src/Form/WordEditFormType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use App\Entity\User;
 use App\Entity\WordList;
-use App\Repository\WordListRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
