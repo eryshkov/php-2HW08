11b79d548d84268f548127fa57b20f772bd2a634
Evgenii Ryshkov <eryshkov@gmail.com>
2019-05-09 15:01:52 +0300

    WordEditFormType.php fixed

--- src/Controller/WordEditController.php
+++ src/Controller/WordEditController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Form\WordEditFormModel;
 use App\Form\WordEditFormType;
 use App\Repository\WordRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,7 +21,7 @@ class WordEditController extends BaseController
     /**
      * @Route("/word/{id}/edit", name="app_word_edit")
      */
-    public function index(int $id, WordRepository $wordRepository, Request $request): Response
+    public function index(int $id, WordRepository $wordRepository, Request $request, EntityManagerInterface $entityManager): Response
     {
         $word = $wordRepository->findOneBy([
             'id'   => $id,
@@ -41,8 +42,20 @@ class WordEditController extends BaseController
         $form->handleRequest($request);
     
         if ($form->isSubmitted() && $form->isValid()) {
+            /** @var WordEditFormModel $wordModelSubmitted */
             $wordModelSubmitted = $form->getData();
-            dd($wordModelSubmitted);
+            $word->setEnglish($wordModelSubmitted->english);
+            $word->setRussian($wordModelSubmitted->russian);
+            $word->setList($wordModelSubmitted->list);
+    
+            $entityManager->persist($word);
+            $entityManager->flush();
+    
+            $this->addFlash('success', 'Слово успешно изменено');
+            return new RedirectResponse($this->generateUrl('app_words_list', [
+                'id' => $word->getList()->getId(),
+                
+            ]));
         }
         
         return $this->render('word_edit/index.html.twig', [
