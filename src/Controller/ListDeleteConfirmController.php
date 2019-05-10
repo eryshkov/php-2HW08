29c2381827a0e364861a6c8452ceb54fa503e992
Evgenii Ryshkov <eryshkov@gmail.com>
2019-05-10 22:56:05 +0300

    ListDeleteConfirmController.php added

---
+++ src/Controller/ListDeleteConfirmController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\WordListRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class ListDeleteConfirmController
+ * @IsGranted("ROLE_USER")
+ */
+class ListDeleteConfirmController extends BaseController
+{
+    /**
+     * @Route("/list/{id}/delete/confirm", name="app_list_delete_confirm")
+     */
+    public function index(int $id, WordListRepository $wordListRepository)
+    {
+        $list = $wordListRepository->findOneBy([
+            'id'   => $id,
+            'user' => $this->getUser(),
+        ]);
+    
+        if (!isset($list)) {
+            $this->addFlash('error', 'Невозможно удалить этот список');
+            return $this->redirectToRoute('app_lists');
+        }
+        
+        return $this->render('list_delete_confirm/index.html.twig', [
+            'listForDeletion' => $list,
+        ]);
+    }
+}
