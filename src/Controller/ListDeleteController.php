0c6f670e35508406753a716145acf69c3c896d6d
Evgenii Ryshkov <eryshkov@gmail.com>
2019-05-10 19:23:51 +0300

    ListDeleteController.php fixed

--- src/Controller/ListDeleteController.php
+++ src/Controller/ListDeleteController.php
@@ -4,9 +4,14 @@ namespace App\Controller;
 
 use App\Repository\WordListRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class ListDeleteController
+ * @IsGranted("ROLE_USER")
+ */
 class ListDeleteController extends BaseController
 {
     /**
