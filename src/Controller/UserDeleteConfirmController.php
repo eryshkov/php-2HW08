6bf4b0f2b47c75076df61884bf5d7881f59cf58e
Evgenii Ryshkov <eryshkov@gmail.com>
2019-05-10 23:14:23 +0300

    UserDeleteConfirmController.php added

---
+++ src/Controller/UserDeleteConfirmController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class UserDeleteConfirmController
+ * @IsGranted("ROLE_ADMIN")
+ */
+class UserDeleteConfirmController extends BaseController
+{
+    /**
+     * @Route("/user/{id}delete/confirm", name="app_user_delete_confirm")
+     */
+    public function index(int $id, UserRepository $userRepository)
+    {
+        $user = $userRepository->findOneBy([
+            'id' => $id,
+        ]);
+    
+        if (!isset($user)) {
+            $this->addFlash('error', 'Пользователь не может быть удален');
+            return $this->redirectToRoute('app_admin');
+        }
+        
+        return $this->render('user_delete_confirm/index.html.twig', [
+            'userForDeletion' => $user,
+        ]);
+    }
+}
