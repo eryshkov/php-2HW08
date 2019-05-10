539f02ef0a93e8f8302d6642766717b170960d63
Evgenii Ryshkov <eryshkov@gmail.com>
2019-05-10 11:35:29 +0300

    ListDeleteController.php added

---
+++ src/Controller/ListDeleteController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\WordListRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ListDeleteController extends BaseController
+{
+    /**
+     * @Route("/list/{id}/delete", name="app_list_delete")
+     */
+    public function index(int $id, WordListRepository $wordListRepository, EntityManagerInterface $entityManager): Response
+    {
+        $list = $wordListRepository->findOneBy([
+            'id'   => $id,
+            'user' => $this->getUser(),
+        ]);
+        
+        if (!isset($list)) {
+            $this->addFlash('error', 'Невозможно удалить этот список');
+            return new RedirectResponse($this->generateUrl('app_lists'));
+        }
+        
+        $entityManager->remove($list);
+        $entityManager->flush();
+        
+        $this->addFlash('success', 'Список ' . $list->getName() . ' успешно удален');
+        
+        return new RedirectResponse($this->generateUrl('app_lists'));
+    }
+}
